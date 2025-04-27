e8a1cc49ef3b9322bc384290a5df1b3152cb4adc
Sure Mourya Sashank <53688863+mouryasashank@users.noreply.github.com>
2025-04-27 12:57:57 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const typed = new Typed('#typed', {
-    strings: ['AI Specialist', 'Data Scientist', 'LLM Engineer', 'Generative AI Enthusiast'],
-    typeSpeed: 50,
-    backSpeed: 30,
-    loop: true
-  });
-});
